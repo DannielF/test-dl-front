6c7e0607f6f80a2c8abdeb2d5bd69c65490b2b1d
daniel.granados <daniel.granados@sofka.com.co>
2023-05-24 16:09:22 -0500

    add: bootstrap cdn links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      ></meta>
+      <body className={roboto.className}>
+        {children}
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+          crossOrigin="anonymous"
+          async
+        ></script>
+      </body>
     </html>
   );
 }
