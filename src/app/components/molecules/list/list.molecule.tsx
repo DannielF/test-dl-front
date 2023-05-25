1e2d28a58ea5d349ad7d69f31e1236f8b5bfe36b
daniel.granados <daniel.granados@sofka.com.co>
2023-05-24 19:21:14 -0500

    upd: list and ul

--- src/app/components/molecules/list/list.molecule.tsx
+++ src/app/components/molecules/list/list.molecule.tsx
@@ -1,22 +1,25 @@
+import Link from 'next/link';
 import styles from './list.module.css';
 
 export type ListMoleculeProps = {
   anchorName: string;
+  routerPath: string;
 };
 
 const classCss = `"nav-item list-group-item" ${styles.list_bg_color}`;
 
-export default function ListMolecule({ anchorName }: ListMoleculeProps) {
+export default function ListMolecule({
+  anchorName,
+  routerPath
+}: ListMoleculeProps) {
   return (
     <li className={classCss}>
-      <a
-        href="#"
-        className="nav-link link-body-emphasis"
-        aria-current="page"
-        data-bcup-haslogintext="no"
+      <Link
+        href={routerPath}
+        className="d-flex align-items-center mt-4 mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
       >
         {anchorName}
-      </a>
+      </Link>
     </li>
   );
 }
--- src/app/components/organisms/ul/ul.organism.tsx
+++ src/app/components/organisms/ul/ul.organism.tsx
@@ -3,14 +3,14 @@ import ListMolecule from '../../molecules/list/list.molecule';
 export default function UlOrganism() {
   return (
     <ul className="nav nav-pills flex-column mb-auto mt-4 list-group list-group-flush">
-      <ListMolecule anchorName="List user" />
-      <ListMolecule anchorName="Get by id" />
-      <ListMolecule anchorName="Get by document" />
-      <ListMolecule anchorName="Get by email" />
-      <ListMolecule anchorName="Create user" />
-      <ListMolecule anchorName="Add user rol" />
-      <ListMolecule anchorName="Update user" />
-      <ListMolecule anchorName="Delete user" />
+      <ListMolecule anchorName="List user" routerPath="list-users" />
+      <ListMolecule anchorName="Get by id" routerPath="get-one" />
+      <ListMolecule anchorName="Get by document" routerPath="get-document" />
+      <ListMolecule anchorName="Get by email" routerPath="get-email" />
+      <ListMolecule anchorName="Create user" routerPath="create-user" />
+      <ListMolecule anchorName="Add user rol" routerPath="add-rol" />
+      <ListMolecule anchorName="Update user" routerPath="update-user" />
+      <ListMolecule anchorName="Delete user" routerPath="delete-user" />
     </ul>
   );
 }
